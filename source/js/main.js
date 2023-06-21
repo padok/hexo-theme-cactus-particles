8be56e2ead4013100edab7f375f6bebdbef12305
Louis Frank <singulosta@gmail.com>
2023-06-21 04:41:28 +0200

    add particles and blur WIP

--- source/js/main.js
+++ source/js/main.js
@@ -110,4 +110,8 @@ $(document).ready(function() {
       });
     }
   }
+
+  particlesJS.load('particles-js', '/assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
 });
